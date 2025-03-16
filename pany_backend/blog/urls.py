37bf14a8e15691d67591f680f93adf8b2cc7cc6d
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-03-17 01:49:56 +0300

    Добавлен вывод отдельного поста через метод get в views. Добавлен шаблон для отображения поста

--- pany_backend/blog/urls.py
+++ pany_backend/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from blog.views import get_posts
+from blog.views import get_posts, get_post
 
 
 app_name = 'blog'
 
 
 urlpatterns = [
-    path('', get_posts, name='posts')
+    path('', get_posts, name='posts'),
+    path('<int:post_id>/', get_post, name='post')
 ]
--- pany_backend/blog/views.py
+++ pany_backend/blog/views.py
@@ -9,3 +9,11 @@ def get_posts(request):
         'posts': posts
     }
     return render(request, 'blog/posts.html', context)
+
+
+def get_post(request, post_id: int):
+    post = Blog.objects.get(pk=post_id)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post.html', context)
