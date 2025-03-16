5b56f3d9145d15deafc620eeb9994f59fae42efd
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-03-16 23:02:40 +0300

    Добавлена модель для поста

--- pany_backend/blog/models.py
+++ pany_backend/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog/')
+    likes = models.PositiveIntegerField(default=0)
+    created_at = models.DateField(auto_now_add=True)
